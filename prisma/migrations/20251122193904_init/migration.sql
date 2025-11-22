1ed6c94da07db8d0954b2ce129e5cc8447566f55
KIMMINJI <amber_lucy@naver.com>
2025-11-23 04:39:20 +0900

    feat : db 스키마 재설정

--- prisma/migrations/20251122193904_init/migration.sql
+++ prisma/migrations/20251122193904_init/migration.sql
@@ -25,8 +25,19 @@ CREATE TABLE `challenge` (
 CREATE TABLE `challenge_participant` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `imageUrl` VARCHAR(191) NULL,
     `userId` INTEGER NOT NULL,
+    `challengeId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `snap` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `imageUrl` VARCHAR(191) NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NULL,
     `challengeId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
@@ -37,3 +48,9 @@ ALTER TABLE `challenge_participant` ADD CONSTRAINT `challenge_participant_userId
 
 -- AddForeignKey
 ALTER TABLE `challenge_participant` ADD CONSTRAINT `challenge_participant_challengeId_fkey` FOREIGN KEY (`challengeId`) REFERENCES `challenge`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `snap` ADD CONSTRAINT `snap_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `snap` ADD CONSTRAINT `snap_challengeId_fkey` FOREIGN KEY (`challengeId`) REFERENCES `challenge`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
