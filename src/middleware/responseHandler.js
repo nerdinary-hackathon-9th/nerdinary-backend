fab6a18457b97857b5c483f9cfb8517524dd2e7a
KIMMINJI <amber_lucy@naver.com>
2025-11-22 22:18:43 +0900

    chore : 응답처리 핸들링

---
+++ src/middleware/responseHandler.js
@@ -0,0 +1,9 @@
+function successHandler(res, message = '성공', data = null) {
+    return res.status(200).json({
+      success: true,
+      message,
+      data
+    });
+}
+  
+export default { successHandler };
